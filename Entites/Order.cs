677517c790b6eeb093e57c1260cffbad9dcd609d
joaoovtms <joaovictormoreirasarmento@gmail.com>
2023-10-05 12:44:18 -0300

    Adicionando projeto

---
+++ Entites/Order.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnumComp.Entites.Enum;
+
+namespace EnumComp.Entites
+{
+    internal class Order
+    {
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Itens { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+        public Order(DateTime date, OrderStatus status, Client client)
+        {
+            Date = date;
+            Status = status;
+            Client = client;
+        }
+
+        public void addItem(OrderItem item) 
+        {
+            Itens.Add(item);
+        }
+        public void removeItem(OrderItem item)
+        {
+            Itens.Remove(item);
+        }
+        public double Total()
+        {
+            double total = 0.0;
+            foreach(OrderItem item in Itens)
+            {
+                total += item.SubTotal();
+            }
+            return total;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order Moment: ");
+            sb.AppendLine(Date.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.Append("Order status: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.AppendLine(Client.ToString());
+            sb.AppendLine("Order items:");
+            foreach(OrderItem item in Itens)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.Append("Total Price: ");
+            sb.AppendLine(Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using EnumComp.Entites;
+using EnumComp.Entites.Enum;
+using System;
+using System.Globalization;
+using System.Transactions;
+
+namespace EnumComp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = EntradaUsu();
+            Console.Write("Email: ");
+            string email = EntradaUsu();
+            Console.Write("Brith date (DD/MM/YYYY): ");
+            DateTime birthdate = DateTime.Parse(EntradaUsu());
+            Client client = new Client(name, email, birthdate);
+            Console.WriteLine();
+            Console.WriteLine("Enter order date: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(EntradaUsu());
+            Order order = new Order(DateTime.Now, status, client);
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(EntradaUsu());
+            Console.WriteLine();
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data");
+                Console.Write("Product name: ");
+                string productname = EntradaUsu();
+                Console.Write("Product price: ");
+                double price = double.Parse(EntradaUsu(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(EntradaUsu());
+                Product product = new Product(productname, price);
+                OrderItem item = new OrderItem(quantity, price, product);
+                order.addItem(item);
+                Console.WriteLine();
+            }
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+
+        }
+
+        static string EntradaUsu()
+        {
+            string? texto = Console.ReadLine();
+            return texto == "" || texto == null ? "0" : texto;
+        }
+    }
+}
